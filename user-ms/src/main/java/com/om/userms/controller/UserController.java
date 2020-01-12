0e7b20cb5e2ca2b079b0a547b99ebe147bc1838f
gaurav singh <tosgaurav@gmail.com>
2020-01-12 20:23:12 +0530

    added account microservice

--- user-ms/src/main/java/com/om/userms/controller/UserController.java
+++ user-ms/src/main/java/com/om/userms/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.om.userms.controller;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@NoArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/users/")
 public class UserController {
 
-    public ResponseEntity<String> get(String userId) {
-        return ResponseEntity.ok("gaurav");
+    @GetMapping(path = "/{userId}", produces = {MediaType.ALL_VALUE})
+    public ResponseEntity<Object> get(String userId) {
+        Map<String, String> user = new HashMap<>();
+        user.put("name", "gaurav");
+        user.put("mobile","8867846411");
+        return ResponseEntity.ok(user);
     }
 }
